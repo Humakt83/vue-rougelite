97c7b892df697031174d6ece78458f34b4f992da
otto <otto@ottos-MacBook.local>
2018-04-14 11:22:41 +0300

    Map component added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    @@@ ROGUE @@@
+    <Map />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Map
   }
 }
 </script>
