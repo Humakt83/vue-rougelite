fb182448f0eae4a4c76994dfdb32aa6c708a1a10
Risto Salama <risto.salama@gofore.com>
2018-04-14 18:49:12 +0300

    Change combat log message

--- src/logic/combat.js
+++ src/logic/combat.js
@@ -1,7 +1,7 @@
 export default (attacker, defender, gameLog) => {
   const multiplier = attacker.attack / defender.defense;
-  const damage = Math.max(attacker.minDamage, Math.floor(Math.random() * attacker.maxDamage)) * multiplier;
-  defender.health -= Math.floor(Math.max(1, damage));
+  const damage = Math.floor(Math.max(1, Math.max(attacker.minDamage, Math.floor(Math.random() * attacker.maxDamage)) * multiplier));
+  defender.health -= damage;
   gameLog.unshift(defender.isPlayer
     ? `Player received ${damage} damage from ${attacker.monsterType}.`
     : `Player dealt ${damage} to ${defender.monsterType}.`
