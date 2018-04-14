1f88acdb56e3ecf4bae07ea039e43e761c549575
Aki Seppänen <aki.seppanen@gofore.com>
2018-04-14 13:08:39 +0300

    Add keydown listener

---
+++ src/Engine.js
@@ -0,0 +1,14 @@
+export default class Engine {
+  moveDown() {
+    console.log('Move Down');
+  }
+  moveUp() {
+    console.log('Move Up');
+  }
+  moveLeft() {
+    console.log('Move Left');
+  }
+  moveRight() {
+    console.log('Move Right');
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-
+import Engine from './Engine'
 Vue.config.productionTip = false
+
+const engine = new Engine()
+
+const keyCommands = {
+  'ArrowLeft': engine.moveLeft,
+  'ArrowRight': engine.moveRight,
+  'ArrowUp': engine.moveUp,
+  'ArrowDown': engine.moveDown,
+}
+
+window.addEventListener('keydown', (e) => {
+  const {key, shiftKey, ctrlKey, altKey} = e
+
+  const modifiers = shiftKey || ctrlKey || altKey
+
+  if (!modifiers && keyCommands.hasOwnProperty(key) && typeof keyCommands[key] === 'function') {
+    const cmd = keyCommands[key]
+
+    cmd()
+  }
+})
 
 new Vue({
   store,
