b603d05c76c152d98300576e7f9cd2261f9e4285
Humakt83 <urlact@yahoo.com>
2019-03-09 00:36:11 +0200

    Make paperdoll styles

--- src/components/info/Inventory.vue
+++ src/components/info/Inventory.vue
@@ -2,6 +2,18 @@
   <div class="inventory">
     <h3>Inventory</h3>
     <div class="paperdoll">
+      <div class="head">
+        <i class="fas fa-hard-hat"/>
+      </div>
+      <div class="hand hand--left">
+        <i class="far fa-hand-paper" />
+      </div>
+      <div class="torso">
+        <i class="fas fa-tshirt" />
+      </div>
+      <div class="hand hand--right">
+        <i class="far fa-hand-paper" />
+      </div>
     </div>
     <div class="backpack">
       <div class="backpackSlot" v-for="(item, index) in inventory.backpack" :key="index">
@@ -21,8 +33,51 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
   .paperdoll {
-    display: inline-block;
-    width: 80%;
+    display: grid;
+    grid-template-columns: 40px 40px 40px;
+    grid-template-rows: auto;
+    grid-template-areas: 
+      ". head ."
+      "left torso right";
+    padding: 1rem 2rem;
+    div {
+      border-radius: 2px;
+      width: 40px;
+      height: 40px;
+      padding: 0.3rem 0.2rem;
+      text-align: center;
+      cursor: pointer;
+      i {        
+        margin-top: 0.3rem;
+        font-size: 30px;
+        font-weight: bolder;
+        color: lightgrey;
+      }
+      &:hover {
+        background-color: whitesmoke;
+        i {
+          color: darkgrey;
+        }
+      }
+    }
+    .head {
+      grid-area: head;
+    }
+    .hand--left {
+      grid-area: left;
+      i {
+        transform: rotate(330deg) scale(-1, 1);
+      }
+    }
+    .hand--right {
+      grid-area: right;
+      i {
+        transform: rotate(30deg);
+      }
+    }
+    .torso {
+      grid-area: torso;
+    }
   }
 
   .backpack {
